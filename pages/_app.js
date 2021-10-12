2632e2cbe13ce8fbec13a91e3692be219e2ad551
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-10-12 14:25:13 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from "@chakra-ui/react"
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Header/>
+      <Component {...pageProps} />
+      <Footer />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/build-resume.js
@@ -0,0 +1,12 @@
+import {
+    Container,
+
+  } from '@chakra-ui/react';
+
+  export default function BulildResume() {
+    return (
+      <Container maxW={'5xl'} >
+
+      </Container>
+    );
+  }
