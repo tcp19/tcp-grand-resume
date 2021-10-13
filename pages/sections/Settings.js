3efabddd1d4322ae85496095443073223b0137c0
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-10-13 16:55:38 +0100

    chore: fill cv information

---
+++ pages/sections/Settings.js
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/layout';
+import React from 'react';
+
+export default function Settings() {
+    return (
+        <Box h={'100vh'}>
+
+
+        </Box>
+    )
+}
---
+++ utils/formatDate.js
@@ -0,0 +1,12 @@
+export function formatDate(date) {
+    if (!date) {
+      return "";
+    }
+    const options = {
+      // weekday: 'long',
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  }
