aadacba41e7c06b26893c4577b2f3a767b40c251
oluwatobiloba2000 <ananioluwatobiloba2000@gmail.com>
2021-10-13 19:47:04 +0100

    add sections for form

--- pages/sections/CVForm.js
+++ pages/sections/CVForm.js
@@ -1,9 +1,17 @@
-import { Box, Flex, Text, Stack, Divider, VStack, Textarea, Checkbox, Button, HStack } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Box, Flex, Text, Stack, Divider, VStack, Textarea, Checkbox, Button, HStack, SimpleGrid, Icon } from '@chakra-ui/react';
+import React, { useRef, useState } from 'react';
 import FormInput from '../../components/FormInput';
 import { formatDate } from '../../utils/formatDate';
+import { BsFillBriefcaseFill } from 'react-icons/bs';
+import { GiSpellBook } from 'react-icons/gi';
+import {GoProject} from 'react-icons/go';
+import {BiWorld} from 'react-icons/bi';
+import {MdComputer} from 'react-icons/md';
 
 export default function CVForm() {
+    const workExperienceRef = useRef();
+    const EducationRef = useRef();
+
     // reusable data points
     let emptyWorkExp = {
         title: "",
@@ -86,13 +94,60 @@ export default function CVForm() {
             ...record,
             workExperiences: [...record.workExperiences, emptyWorkExp]
         })
+
+        window.scrollTo(0, workExperienceRef.current.offsetTop)
+    }
+
+    const addEducation = () =>{
+        setRecord({
+            ...record,
+            education: [...record.education, emptyEducation]
+        })
+
+        window.scrollTo(0, EducationRef.current.offsetTop)
     }
 
     return (
         <Box minH={'100vh'} w={'100%'} my="4" >
-            <Flex direction={{ base: 'column-reverse', xl: 'row' }} color="white" >
-                <Box w={{ base: '100%', xl: "25vw" }} minH="80vh" bg="green.500">
-                    {/* <Text>Box 1</Text> */}
+            <Flex direction={{ base: 'column-reverse', xl: 'row' }} color="white">
+                <Box boxShadow="2xl" mr="5px" p="6" rounded="md" w={{ base: '100%', xl: "25vw" }}  bg="#ffffff">
+                  <Text color="#181C27" fontWeight="bold" >Fill Section</Text>
+
+                 {/* section selection container */}
+                  <Box p="0" m="30px 0px">
+                  <SimpleGrid minChildWidth="120px" spacing="10px">
+
+                   { [{
+                        onClick: addWorkExperience,
+                        text: "Add Work Experience",
+                        icon: BsFillBriefcaseFill,
+                    },{
+                        onClick: addEducation,
+                        text: "Add Education",
+                        icon: GiSpellBook,
+                    }, {
+                        onClick: ()=> console.log("add project"),
+                        text: "Add Project",
+                        icon: GoProject,
+                    },{
+                        onClick: ()=> console.log("add language"),
+                        text: "Add Language",
+                        icon: BiWorld,
+                    }, {
+                        onClick: ()=> console.log("add skills"),
+                        text: "Add Skills",
+                        icon: MdComputer,
+                    }].map((val, index)=>
+                        <Flex key={val.text + index} as="button"_hover={{
+                            boxShadow: 'outline'
+                        }} onClick={val.onClick} cursor="pointer" justifyContent="center" flexDirection={"column"} alignItems="center" boxShadow="xs"  rounded="md" height="120px">
+                            <Icon as={val.icon} color="#000000" fontSize="40px"/>
+                            <Text color="#181C27" fontSize="13px" fontWeight="semibold" textAlign={"center"}>{val.text}</Text>
+                        </Flex>
+                     )
+                    }
+                   </SimpleGrid>
+                  </Box>
                 </Box>
 
                 <Box display="flex" flexDirection="column" justifyContent="space-between" align="stretch" flex="1" minH="80vh" p="4">
@@ -124,7 +179,7 @@ export default function CVForm() {
                         </Stack>
                     </VStack>
 
-                    <VStack mt={6} align="start">
+                    <VStack mt={6} ref={workExperienceRef} align="start">
                         <Text color="#181C27" fontWeight="bold" >Work Experience</Text>
                         <Divider />
                     </VStack>
@@ -162,7 +217,7 @@ export default function CVForm() {
                     <Button bg={"#181C27"} _hover={{ bg: "#181C27" }} mt={4} onClick={() => addWorkExperience()}>Remove Work Experience</Button>
 
                     </HStack> */}
-                    <VStack mt={6} align="start">
+                    <VStack mt={6} ref={EducationRef} align="start">
                         <Text color="#181C27" fontWeight="bold" >Education</Text>
                         <Divider />
                     </VStack>
