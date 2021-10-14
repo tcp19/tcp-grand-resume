0541475a8101a3223a46a98fbea5b6b9da75b8f4
mr-emeka <speak2c.emeka@gmail.com>
2021-10-14 10:03:34 +0100

    adds default test

---
+++ utils/formateDate.test.js
@@ -0,0 +1,11 @@
+import {formatDate} from "./formatDate"
+
+
+describe("tests format date",  () => {
+    it("if data isn't pass should return null",async()=>{
+        expect(formatDate()).toBe("")
+    })
+    it("should return invalid date for \"21st october\"",async()=>{
+        expect(formatDate("21st october")).toBe("Invalid Date")
+    })
+})
