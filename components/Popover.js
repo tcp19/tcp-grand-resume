a8f130b298b3c8571faf274b392c890c95b167e0
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-11-13 14:04:40 +0100

    fix lock

--- components/Popover.js
+++ components/Popover.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
     Popover,
     PopoverTrigger,
@@ -12,35 +12,58 @@ import {
     Button,
 } from "@chakra-ui/react";
 
-export function DeletePopOver({triggerPopover, children, onChooseYes }) {
+export function DeletePopOver({ triggerPopover, children, onChooseYes }) {
     return (
-      <>{ triggerPopover === true? <Popover
-            returnFocusOnClose={false}
-            color="#181C27"
-            placement="left"
-            closeOnBlur={true}
-        >
-            {({ isOpen, onClose }) => (
-                <>
-                    <PopoverTrigger>
-                       {children}
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        <PopoverHeader color="#181C27" fontWeight="semibold">Confirmation</PopoverHeader>
-                        <PopoverArrow />
-                        <PopoverCloseButton />
-                        <PopoverBody color="#181C27">
-                            Are you sure you want to delete?
-                        </PopoverBody>
-                        <PopoverFooter d="flex" justifyContent="flex-end">
-                            <ButtonGroup size="sm">
-                                <Button variant="outline" color="#181C27" onClick={onClose}>Cancel</Button>
-                                <Button colorScheme="red" onClick={onChooseYes}>Delete</Button>
-                            </ButtonGroup>
-                        </PopoverFooter>
-                    </PopoverContent>
-                </>
+        <>
+            {triggerPopover === true ? (
+                <Popover
+                    returnFocusOnClose={false}
+                    color="#181C27"
+                    placement="left"
+                    closeOnBlur={true}
+                >
+                    {({ isOpen, onClose }) => (
+                        <>
+                            <PopoverTrigger>{children}</PopoverTrigger>
+                            <PopoverContent>
+                                <PopoverHeader
+                                    color="#181C27"
+                                    fontWeight="semibold"
+                                >
+                                    Confirmation
+                                </PopoverHeader>
+                                <PopoverArrow />
+                                <PopoverCloseButton />
+                                <PopoverBody color="#181C27">
+                                    Are you sure you want to delete?
+                                </PopoverBody>
+                                <PopoverFooter
+                                    d="flex"
+                                    justifyContent="flex-end"
+                                >
+                                    <ButtonGroup size="sm">
+                                        <Button
+                                            variant="outline"
+                                            color="#181C27"
+                                            onClick={onClose}
+                                        >
+                                            Cancel
+                                        </Button>
+                                        <Button
+                                            colorScheme="red"
+                                            onClick={onChooseYes}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </ButtonGroup>
+                                </PopoverFooter>
+                            </PopoverContent>
+                        </>
+                    )}
+                </Popover>
+            ) : (
+                <span onClick={onChooseYes}>{children}</span>
             )}
-        </Popover> : <span onClick={onChooseYes}>{children}</span>}</>
-    )
+        </>
+    );
 }
